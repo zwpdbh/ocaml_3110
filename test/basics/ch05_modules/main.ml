04ea70f90473fa625d07963897b80b2b17efa871
zwpdbh <hyperion_z@outlook.com>
2023-07-02 15:46:53 +0800

    < fixed test errors

--- test/basics/ch05_modules/main.ml
+++ test/basics/ch05_modules/main.ml
@@ -1,16 +1,18 @@
 (* dune exec -- test/basics/ch05_modules/main.exe *)
 
-(* open OUnit2
-open Basics
+open OUnit2
+
+(* open Basics *)
 open Basics.Ch05_modules
 
 let q = ListQueue.(empty |> enqueue 42 |> dequeue >>| enqueue 43 >>= dequeue)
 
-let test q =
+let test_queue =
   match q with
-  | Some x -> x
-  | None -> ListQueue.empty
+  | Some x ->
+    "test queue module"
+    >::: [ ("case01" >:: fun _ -> assert_equal (ListQueue.peek x) None) ]
+  | None -> "test queue module" >::: []
 ;;
 
-let test_queue = "test queue module" >::: [ ("case01" >:: fun _ -> assert_equal 0 0) ]
-let _ = run_test_tt_main test_queue *)
+let _ = run_test_tt_main test_queue
